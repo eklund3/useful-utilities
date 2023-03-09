2b31b6f231e7e6fa2be7e71caf3b080541b373f5
Alavyali <alavyali@msd-spb.ru>
2023-03-09 16:04:20 +0300

    Медианная фильтрация и усреднее

--- Math/filters.cpp
+++ Math/filters.cpp
@@ -4,13 +4,15 @@ namespace math {
 namespace filters {
 double getAvrgInInterval(const QVector<double> &src, int from, int to)
 {
-    return std::accumulate(src.begin()+from, src.begin()+to, 0)/(double)(to-from);
+    if (from - to == 0) return src[from];
+    return std::accumulate(src.begin()+from, src.begin()+to+1, 0.0) / (double)(to-from+1);
 }
 double getMedianInInterval(const QVector<double> &src, int from, int to)
 {
-    std::vector<double> vec(src.begin()+from, src.begin()+to);
-    std::sort(vec.begin()+from, vec.begin()+to);
-    return vec[round(from+to)/2];
+    if (from - to == 0) return src[from];
+    std::vector<double> vec(src.begin()+from, src.begin()+to+1);
+    std::sort(vec.begin(), vec.begin() + vec.size());
+    return vec[(to-from)/2];
 }
 
 void medianFiltr(QVector<double> &src, QVector<double> &dst, int window)
@@ -18,7 +20,7 @@ void medianFiltr(QVector<double> &src, QVector<double> &dst, int window)
     if (window == 0) return;
     if (window < 3) {
         window = 3;
-        qDebug() << "averageFiltr(): Invalid median filter window";
+        qDebug() << "medianFiltr(): Invalid median filter window";
     }
     if (window % 2 == 0) window++;
     QVector<double> src_buf = src;
@@ -26,16 +28,16 @@ void medianFiltr(QVector<double> &src, QVector<double> &dst, int window)
     int start = floor((float)window/2.0);
     int end = src_buf.size() - start;
     double median = getMedianInInterval(src_buf, 0, start-1);
-    for(int i = 0; i < start-1; i++) {
-        dst.push_back(median); //заполняем то, для чего не можем посчитать медиану
+    for(int i = 0; i < start; i++) {
+        dst.push_back(src_buf[i]); //заполняем то, для чего не можем посчитать медиану
     }
     for(int i = start; i < end; i++) {
         median = getMedianInInterval(src_buf,i-start, i+start);
         dst.push_back(median);
     }
-    median = getMedianInInterval(src_buf, end, src_buf.size());
+    median = getMedianInInterval(src_buf, end, src_buf.size()-1);
     for(int i = end; i < src_buf.size(); i++) {
-        dst.push_back(median); //заполняем то, для чего не можем посчитать медиану
+        dst.push_back(src_buf[i]); //заполняем то, для чего не можем посчитать медиану
     }
 }
 
@@ -52,16 +54,16 @@ void averageFiltr(QVector<double> &src, QVector<double> &dst, int window)
     int start = floor((float)window/2.0);
     int end = src_buf.size() - start;
     double avrg = getAvrgInInterval(src_buf, 0, start-1);
-    for(int i = 0; i < start-1; i++) {
-        dst.push_back(avrg); //заполняем то, для чего не можем посчитать среднее
+    for(int i = 0; i < start; i++) {
+        dst.push_back(src_buf[i]); //заполняем то, для чего не можем посчитать среднее
     }
     for(int i = start; i < end; i++) {
         avrg = getAvrgInInterval(src_buf,i-start, i+start);
         dst.push_back(avrg);
     }
-    avrg = getAvrgInInterval(src_buf, end, src_buf.size());
+    avrg = getAvrgInInterval(src_buf, end, src_buf.size()-1);
     for(int i = end; i < src_buf.size(); i++) {
-        dst.push_back(avrg); //заполняем то, для чего не можем посчитать среднее
+        dst.push_back(src_buf[i]); //заполняем то, для чего не можем посчитать среднее
     }
 }
 } //namespace filters
